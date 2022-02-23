1e07a506c387f72157536f5919b035fdce240f29
Krzysztof Cislo <krzysztof.cislo265@gmail.com>
2022-02-23 20:52:37 +0100

    sprites introduced

--- sudoku.py
+++ sudoku.py
@@ -1,113 +1,215 @@
 import pygame
 import sys
 
-from itertools import product
+from pygame.rect import Rect
+from pygame.sprite import AbstractGroup, Group, Sprite
+from pygame.surface import Surface
+from typing import *
 
 
-class Square:
-    def __init__(self, id, pos, value=None) -> None:
-        self.id: int = id
-        self.pos: tuple[int] = pos
-        self.value: int = value
-        self.surf: pygame.Surface = font.render(f'', True, 'Black')
-        self.rect: pygame.Rect = pygame.Rect(self.pos, (80,80))
-        self.selected = False
-        self.set_value(value)
+SQUARE_LEN = 80
+
+
+class Square(Sprite):
+    def __init__(self, *groups: AbstractGroup) -> None:
+        super().__init__(*groups)
+        self.row: int = self.groups()[0].id
+        self.col: int = self.groups()[1].id
+        self.hoovered: bool = False
+        self.selected: bool = False
+        self.locked: bool = False
+        self.value: int = 0
+        self.image: pygame.Surface = font.render(f'', True, 'Black')
+        self.rect = pygame.Rect((self.row*SQUARE_LEN, self.col*SQUARE_LEN), (SQUARE_LEN, SQUARE_LEN))
 
     def __repr__(self) -> str:
-        return f'{self.id}. {self.__class__.__name__}[{int(self.id/9)}, {int(self.id%9)}] = {self.value} at pos {self.rect.center}'
+        return f'{super().__repr__()} | pos = ({self.row},{self.col})'
+
+    def draw(self, surface: Surface) -> None:
+        fill_color = 'White'
+        if self.hoovered and self.selected:
+            fill_color = 'cadetblue'
+        elif self.selected:
+            fill_color = 'cadetblue1'
+        elif self.hoovered or self.locked:
+            fill_color = 'Gray90'
+        pygame.draw.rect(surface, fill_color, self.rect)
+
+    def update(self, *args: Any, **kwargs: Any) -> None:
+        self.update_hoovered()
+        self.update_image()
+        return super().update(*args, **kwargs)
+
+    def update_hoovered(self) -> None:
+        mouse_pos = pygame.mouse.get_pos()
+        self.hoovered = self.rect.collidepoint(mouse_pos)
+
+    def update_image(self) -> None:
+        font_color = 'Gray20'
+        if self.locked:
+            font_color = 'Black'
+        if self.value and (1 <= self.value <= 9):
+            self.image: pygame.Surface = font.render(f'{self.value}', True, font_color)
+        else:
+            self.image: pygame.Surface = font.render(f'', True, font_color)
+
+    def init_value(self, value: int) -> None:
+        if value and (1 <= value <= 9):
+            self.value = value
+            self.locked = True
+
+    def set_value(self, value: int) -> None:
+        if self.locked:
+            return
+        self.value = value
+
+    def clear_value(self) -> None:
+        if self.locked:
+            return
+        self.value = 0
 
     def select(self) -> None:
+        if self.locked:
+            return
         self.selected = True
 
     def deselect(self) -> None:
+        if self.locked:
+            return
         self.selected = False
 
-    def set_value(self, value: int) -> None:
-        # if value is None: # for debug purposes
-        if value is None or not (1 <= value <= 9):
-            return
-        self.value = value
-        self.surf = font.render(f'{self.value}', True, 'Black')
 
-    def clear_value(self) -> None:
-        self.value = None
-        self.surf = font.render(f'', True, 'Black')
+class Row(Group):
+    def __init__(self, id, *sprites: Union[Sprite, Sequence[Sprite]]) -> None:
+        super().__init__(*sprites)
+        self.id = id
+
+    def __repr__(self) -> str:
+        return f'{super().__repr__()} = {self.id}'
+
+    def draw(self, surface: Surface) -> None:
+        if self.id % 3 == 0:
+            pygame.draw.line(surface, 'Black', (0, self.id*SQUARE_LEN), (9*SQUARE_LEN, self.id*SQUARE_LEN), 2)
+        else:
+            pygame.draw.line(surface, 'Black', (0, self.id*SQUARE_LEN), (9*SQUARE_LEN, self.id*SQUARE_LEN), 1)
 
 
-class Mesh:
+class Col(Group):
+    def __init__(self, id, *sprites: Union[Sprite, Sequence[Sprite]]) -> None:
+        super().__init__(*sprites)
+        self.id = id
+
+    def __repr__(self) -> str:
+        return f'{super().__repr__()} = {self.id}'
+
+    def draw(self, surface: Surface) -> None:
+        if self.id % 3 == 0:
+            pygame.draw.line(surface, 'Black', (self.id*SQUARE_LEN, 0), (self.id*SQUARE_LEN, 9*SQUARE_LEN), 2)
+        else:
+            pygame.draw.line(surface, 'Black', (self.id*SQUARE_LEN, 0), (self.id*SQUARE_LEN, 9*SQUARE_LEN), 1)
+
+
+class BigSquare(Group):
+    def __init__(self, id, *sprites: Union[Sprite, Sequence[Sprite]]) -> None:
+        super().__init__(*sprites)
+        self.id = id
+
+    def __repr__(self) -> str:
+        return f'{super().__repr__()} = {self.id}'
+
+
+class Sudoku(Group):
+    def __init__(self, *sprites: Union[Sprite, Sequence[Sprite]]) -> None:
+        super().__init__(*sprites)
+        self.rect = pygame.Rect((0, 0), (9*SQUARE_LEN, 9*SQUARE_LEN))
+
+    def draw_border(self, surface: Surface) -> None:
+        pygame.draw.line(surface, 'Black', self.rect.topleft, self.rect.topright, 6)
+        pygame.draw.line(surface, 'Black', self.rect.topleft, self.rect.bottomleft, 6)
+        pygame.draw.line(surface, 'Black', self.rect.bottomleft, self.rect.bottomright, 10)
+        pygame.draw.line(surface, 'Black', self.rect.topright, self.rect.bottomright, 10)
+
+    def draw(self, surface: Surface) -> List[Rect]:
+        for square in self.sprites():
+            square.draw(surface)
+        self.draw_border(surface)
+        return super().draw(surface)
+
+
+class SudokuWrapper:
     def __init__(self) -> None:
-        self.square_list: list[Square] = []
-        for i, (y, x) in enumerate(product(range(9), range(9))):
-            pos = (80*x, 80*y)
-            # self.square_list.append(Square(i, pos, i)) # for debug purposes
-            self.square_list.append(Square(i, pos))
+        self.sudoku: Sudoku = Sudoku()
+        self.rows: list[Row] = []
+        self.cols: list[Col] = []
+        self.big_squares: list[BigSquare] = []
+        self.squares: list[Square] = []
+        for i in range(9):
+            self.rows.append(Row(i))
+            self.cols.append(Col(i))
+            self.big_squares.append(BigSquare(i))
+        for x in range(9):
+            for y in range(9):
+                self.squares.append(Square(self.rows[y], self.cols[x], self.big_squares[int(x/3)+int(y/3)*3], self.sudoku))
 
-    def blit(self) -> None:
-        for square in self.square_list:
-            if square.selected:
-                pygame.draw.rect(screen, 'Gray', square.rect)
-            else:
-                pygame.draw.rect(screen, 'White', square.rect)
-            pygame.draw.rect(screen, 'Black', square.rect, 2)
-            pygame.draw.rect(screen, 'Black', screen.get_rect(), 5)
-            screen.blit(square.surf, square.rect)
+    def draw(self) -> None:
+        self.sudoku.draw(screen)
+        for row in self.rows:
+            row.draw(screen)
+        for col in self.cols:
+            col.draw(screen)
+
+    def update(self) -> None:
+        self.sudoku.update()
 
     def get_collide_square(self, pos: tuple[int]) -> Square | None:
-        for square in self.square_list:
+        for square in self.squares:
             if square.rect.collidepoint(pos):
                 return square
         return None
 
     def get_selected_square(self) -> Square | None:
-        for square in self.square_list:
+        for square in self.squares:
             if square.selected:
                 return square
         return None
 
-    def get_forbidden_values(self) -> list:
-        forbidden_values = []
-        sel_square = self.get_selected_square()
-        # vertical
-        for id in range(sel_square.id % 9, 81, 9):
-            if (value:=self.square_list[id].value) is not None:
-                forbidden_values.append(value)
-        # horizontal
-        beg_id = sel_square.id - sel_square.id % 9
-        for id in range(beg_id, beg_id+9):
-            if (value:=self.square_list[id].value) is not None:
-                forbidden_values.append(value)
-        # big square
-        beg_id = sel_square.id - sel_square.id % 3
-        beg_id -= (sel_square.id - sel_square.id % 9) % 27
-        for id in range(beg_id, beg_id+3):
-            if (value:=self.square_list[id].value) is not None:
-                forbidden_values.append(value)
-        beg_id += 9
-        for id in range(beg_id, beg_id+3):
-            if (value:=self.square_list[id].value) is not None:
-                forbidden_values.append(value)
-        beg_id += 9
-        for id in range(beg_id, beg_id+3):
-            if (value:=self.square_list[id].value) is not None:
-                forbidden_values.append(value)
-        return forbidden_values
-
     def value_valid(self, value: int) -> bool:
-        if value in self.get_forbidden_values():
+        selected_square = self.get_selected_square()
+        forbidden_values = set()
+        for group in selected_square.groups():
+            if type(group) == Sudoku:
+                continue
+            for sprite in group.sprites():
+                forbidden_values.add(sprite.value)
+        if value in forbidden_values:
             return False
         return True
 
+    def init(self, values: list[int]) -> None:
+        for value, square in zip(values, self.squares):
+            square.init_value(value)
 
 
 pygame.init()
 pygame.display.set_caption('Sudoku')
-screen = pygame.display.set_mode((720, 720))
+screen = pygame.display.set_mode((9*SQUARE_LEN, 9*SQUARE_LEN))
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 100)
 
-mesh = Mesh()
-# print(mesh.square_list)
+sudoku = SudokuWrapper()
+sudoku.init(
+    [
+        1,2,3,4,5,6,7,8,9,
+        0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,
+    ]
+)
 
 while True:
     for event in pygame.event.get():
@@ -115,25 +217,30 @@ while True:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == pygame.BUTTON_LEFT:
-            if (square := mesh.get_selected_square()):
-                square.deselect()
-            if (square := mesh.get_collide_square(event.pos)):
-                square.select()
+            selected_square = sudoku.get_selected_square()
+            pressed_square = sudoku.get_collide_square(event.pos)
+            if pressed_square and selected_square == pressed_square:
+                pressed_square.deselect()
+            else:
+                if selected_square:
+                    selected_square.deselect()
+                if pressed_square:
+                    pressed_square.select()
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == pygame.BUTTON_RIGHT:
-            if (square := mesh.get_selected_square()):
-                square.clear_value()
+            if (square := sudoku.get_selected_square()):
                 square.deselect()
         elif event.type == pygame.KEYDOWN:
-            if (pygame.K_1 <= event.key <= pygame.K_9) and (square := mesh.get_selected_square()):
+            if (pygame.K_1 <= event.key <= pygame.K_9) and (square := sudoku.get_selected_square()):
                 value = int(chr(event.key))
-                if mesh.value_valid(value):
+                if sudoku.value_valid(value):
                     square.set_value(value)
                 square.deselect()
-            elif event.key == pygame.K_DELETE and (square := mesh.get_selected_square()):
+            elif event.key == pygame.K_DELETE and (square := sudoku.get_selected_square()):
                 square.clear_value()
                 square.deselect()
 
-    mesh.blit()
+    sudoku.update()
+    sudoku.draw()
 
     pygame.display.update()
     clock.tick(60)
